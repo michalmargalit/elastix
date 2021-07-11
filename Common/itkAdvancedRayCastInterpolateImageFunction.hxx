dee212223a524154c3b42e2462b8180b8bfdfe32
Michal Margalit <michal@rsipvision.com>
2021-07-11 12:49:39 +0300

    BUG: RayCastInterpolateImageFunciton physical space issues

--- Common/itkAdvancedRayCastInterpolateImageFunction.hxx
+++ Common/itkAdvancedRayCastInterpolateImageFunction.hxx
@@ -752,11 +752,11 @@ RayCastHelper<TInputImage, TCoordRep>::SetRay(OutputPointType RayPosn, Direction
   m_VoxelDimensionInY = spacing[1];
   m_VoxelDimensionInZ = spacing[2];
 
-  m_CurrentRayPositionInMM[0] = RayPosn[0] + 0.5 * m_VoxelDimensionInX * (double)m_NumberOfVoxelsInX;
+  m_CurrentRayPositionInMM[0] = RayPosn[0];
 
-  m_CurrentRayPositionInMM[1] = RayPosn[1] + 0.5 * m_VoxelDimensionInY * (double)m_NumberOfVoxelsInY;
+  m_CurrentRayPositionInMM[1] = RayPosn[1];
 
-  m_CurrentRayPositionInMM[2] = RayPosn[2] + 0.5 * m_VoxelDimensionInZ * (double)m_NumberOfVoxelsInZ;
+  m_CurrentRayPositionInMM[2] = RayPosn[2];
 
   m_RayDirectionInMM[0] = RayDirn[0];
   m_RayDirectionInMM[1] = RayDirn[1];
@@ -1530,7 +1530,7 @@ AdvancedRayCastInterpolateImageFunction<TInputImage, TCoordRep>::Evaluate(const 
   ray.ZeroState();
   ray.Initialise();
 
-  ray.SetRay(point, direction);
+  ray.SetRay(point - this->m_Image->GetOrigin().GetVectorFromOrigin(), direction);
   ray.IntegrateAboveThreshold(integral, m_Threshold);
 
   return (static_cast<OutputType>(integral));
